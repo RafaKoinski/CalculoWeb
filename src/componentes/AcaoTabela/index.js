56745016ae998e6b3c8a2ae5269997638b35b184
Rafael Koinski <rkoinski@gmail.com>
2025-01-17 15:02:55 -0300

    criando uma modal

--- src/componentes/AcaoTabela/index.js
+++ src/componentes/AcaoTabela/index.js
@@ -1,18 +1,43 @@
-
-import { Link } from "react-router-dom"; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import  style  from "./AcaoTabela.module.css";
-import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
-
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
 function AcaoTabela() {
     
+   const [show, setShow] = useState(false);
+
+   const ocultar = () => setShow(false);
+   const mostrar = () => setShow(true);
+    
     return(
+
         <td>
             <nav className={style.acao}>
-                <Link className={style.link} href="#"><FontAwesomeIcon className={style.icone} icon={faPen} /></Link>
+                <Link className={style.link} onClick={mostrar}>
+                    <FontAwesomeIcon className={style.icone} icon={faPen} />
+                </Link>
                 <Link className={style.link} href="#"><FontAwesomeIcon className={style.icone} icon={faTrashCan} /></Link>
             </nav>
+               
+            <Modal show={show} onHide={ocultar}>
+                <Modal.Header>
+                    <Modal.Title>Editar entradas</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Formulário ainda está por vir</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={ocultar}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={ocultar}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </td>
     )
 }
